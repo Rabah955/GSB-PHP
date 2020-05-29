4152dcf6f492fc58a0201e10bc55bed90ec9d32e
Rabah <rabah@MacBook-Pro-de-Rabah.local>
2020-05-30 00:50:44 +0200

    Project GSB

--- views/accueil.php
+++ views/accueil.php
@@ -29,6 +29,15 @@
             <div class="panel-body">
                 <div class="row">
                     <div class="col-xs-12 col-md-12">
+                        <p>Bonjour <?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>, bienvenu sur l'application de suivi du remboursement des frais. Ici, vous pourrez saisir les quantités de frais forfaitisés et les frais hors forfait engagés pour le mois écoulé.
+                            Vous aurez accès en modification à la fiche tout au long du mois et vous pourrez y ajouter de nouvelles données ou supprimer des éléments saisis.
+                            Les frais saisis peuvent remonter jusqu’à un an en arrière.
+                        </p>
+                        <p>La fiche est clôturée au dernier jour du mois. Cette clôture sera réalisée par l’application selon l’une des modalités suivantes :
+                        <li>A la première saisie pour le mois N par le visiteur, sa fiche du mois précédent est clôturée si elle ne l’est pas</li>
+                        <li>Au début de la campagne de validation des fiches par le service comptable, un script est lancé qui clôture toutes les fiches non clôturées du mois qui va être traité</li></p>
+
+                        <p>Nous restons à votre disposition pour toutes autres questions.</p>
                         <a href="index.php?uc=gererFrais&action=saisirFrais"
                            class="btn btn-success btn-lg" role="button">
                             <span class="glyphicon glyphicon-pencil"></span>
